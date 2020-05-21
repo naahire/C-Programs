06c215ce7e817461b8d3eab22f61e9b25ac00fdd
naahire <65179560+naahire@users.noreply.github.com>
2020-05-21 19:19:40 +0530

    Add files via upload

---
+++ Nile.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+
+    float x[100],y[100],a,b,r,c;
+    int i,n;
+  printf("Enter The no of points ");
+  scanf("%d",&n);  
+  printf("\n");
+   printf("Enter The no of Radius ");
+  scanf("%f",&r);
+  /*a=((2*3.14)/n);
+  printf("\nThe angle of rotation is%f",a);
+ x[0]=r;
+ y[0]=0;
+ printf(" The points are ");
+ for(i=0;i<=n;i++)
+ {
+  x[i+1]=x[i]*cos(a)-y[i]*sin(a);
+  y[i+1]=x[i]*sin(a)-y[i]*cos(a);
+  printf("\n%f\t%f ",x[i],y[i]);
+ }*/
+}
